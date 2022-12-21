332feb0378b9685da7916ea07c20c8eaff0f2b37
Martín <martinrabasa@gmail.com>
2022-12-21 16:58:44 -0300

    update

--- pages/invitados.tsx
+++ pages/invitados.tsx
@@ -21,14 +21,14 @@ export default function Obra() {
             <div className="flex flex-col gap-6 text-primary-900 mx-4 my-8 md:mx-10 lg:mx-14 xl:mx-44 mb-auto">
                 <div className="flex flex-col gap-6 mb-6">
                     <h1 className="font-asap text-3xl">Espacio para invitados</h1>
-                    <p className="font-roboto font-light text-base text-primary-700 leading-8">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi at hic magni ipsa facilis accusamus maxime. Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima consectetur nostrum porro doloribus quis expedita dolorem, ab, veritatis dolores.</p>
+                    <p className="font-roboto font-light text-base text-primary-700 leading-8 text-justify">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi at hic magni ipsa facilis accusamus maxime. Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima consectetur nostrum porro doloribus quis expedita dolorem, ab, veritatis dolores.</p>
                 </div>
                 <div className="group flex flex-col gap-3">
                     <Link href="#" className="self-start">
                         <h2 className="font-asap text-2xl group-hover:text-primary-700">Titulo titulo</h2>
                     </Link>
                     <Link href="#">
-                        <p className="font-roboto font-light text-md text-primary-700 leading-8">
+                        <p className="font-roboto font-light text-md text-primary-700 leading-8 text-justify">
                             Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi at hic magni ipsa facilis accusamus maxime. Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima consectetur nostrum porro doloribus quis expedita dolorem, ab, veritatis dolores...</p>
                     </Link>
                     <Link className="flex gap-2 items-center justify-end text-sm text-secondary-600 hover:text-secondary-400" href="#">
