7ae052f4e3b50fd252e904035be7b2628caed4fd
Martín <martinrabasa@gmail.com>
2022-12-22 22:56:08 -0300

    analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,25 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { AuthProvider } from "../utils/Auth";
 import { CommentsProvider } from "../utils/Comments";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
         <AuthProvider>
             <CommentsProvider>
+                <Script
+                    src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+                    strategy="afterInteractive"
+                />
+                <Script id="google-analytics" strategy="afterInteractive">
+                    {`
+                        window.dataLayer = window.dataLayer || [];
+                        function gtag(){window.dataLayer.push(arguments);}
+                        gtag('js', new Date());
+
+                        gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
+                    `}
+                </Script>
                 <Component {...pageProps} />
             </CommentsProvider>
         </AuthProvider>
--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,4 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
-import Script from "next/script";
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -11,19 +10,6 @@ class MyDocument extends Document {
     return (
       <Html lang="es">
         <Head>
-          <Script
-            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-            strategy="afterInteractive"
-          />
-          <Script id="google-analytics" strategy="afterInteractive">
-            {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){window.dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
-        `}
-          </Script>
           <link
             rel="icon"
             href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🍂</text></svg>"
