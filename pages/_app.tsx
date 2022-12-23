1f84151b1838f06d309e39fb6945f1259b255a68
Martín <martinrabasa@gmail.com>
2022-12-22 23:09:47 -0300

    ganalytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,26 +1,51 @@
 import "../styles/globals.css";
+import { useEffect } from 'react'
 import type { AppProps } from "next/app";
 import { AuthProvider } from "../utils/Auth";
 import { CommentsProvider } from "../utils/Comments";
 import Script from "next/script";
+import { useRouter } from 'next/router'
+import * as gtag from '../lib/gtag'
 
 function MyApp({ Component, pageProps }: AppProps) {
+    const router = useRouter()
+
+    useEffect(() => {
+        const handleRouteChange = (url: any) => {
+            gtag.pageview(url)
+        }
+
+        router.events.on('routeChangeComplete', handleRouteChange)
+        router.events.on('hashChangeComplete', handleRouteChange)
+
+        return () => {
+            router.events.off('routeChangeComplete', handleRouteChange)
+            router.events.off('hashChangeComplete', handleRouteChange)
+        }
+    }, [router.events])
+
     return (
         <AuthProvider>
             <CommentsProvider>
-                <Script
-                    src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-                    strategy="afterInteractive"
-                />
-                <Script id="google-analytics" strategy="afterInteractive">
-                    {`
-                        window.dataLayer = window.dataLayer || [];
-                        function gtag(){window.dataLayer.push(arguments);}
-                        gtag('js', new Date());
-
-                        gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
-                    `}
-                </Script>
+                {/* Global Site Tag (gtag.js) - Google Analytics */}
+            <Script
+                strategy="afterInteractive"
+                src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+            />
+            <Script
+                id="gtag-init"
+                strategy="afterInteractive"
+                dangerouslySetInnerHTML={{
+                __html: `
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${gtag.GA_TRACKING_ID}', {
+                    page_path: window.location.pathname,
+                    });
+                `,
+                }}
+            />
                 <Component {...pageProps} />
             </CommentsProvider>
         </AuthProvider>
--- pages/_document.js
+++ pages/_document.js
@@ -14,6 +14,16 @@ class MyDocument extends Document {
             rel="icon"
             href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🍂</text></svg>"
           />
+          {/* Global Site Tag (gtag.js) - Google Analytics */}
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+            `,
+            }}
+          />
         </Head>
         <body>
           <Main />
